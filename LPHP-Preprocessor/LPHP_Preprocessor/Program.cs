22e67cbd457007b30a9ec3479940f375627cb0fb
TobiHatti <tobias.hattinger@gmx.at>
2020-05-23 19:00:25 +0200

    Added Directory-Watcher

--- LPHP-Preprocessor/LPHP_Preprocessor/Program.cs
+++ LPHP-Preprocessor/LPHP_Preprocessor/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LPHP_Preprocessor
@@ -10,6 +13,60 @@ namespace LPHP_Preprocessor
     {
         static void Main(string[] args)
         {
+            string watchFolder;
+            try
+            {
+                watchFolder = args[1];
+            }
+            catch(IndexOutOfRangeException)
+            {
+                Console.WriteLine("Could not start the LPHP-Preprocessor. Please provide a path to the target folder.");
+            }
+
+#if DEBUG
+            watchFolder = @"H:\LPHPTest\";
+#endif
+
+            Dictionary<string, string> lphpFiles = new Dictionary<string, string>();
+
+            while (true)
+            {
+                foreach (string filePath in Directory.GetFiles(watchFolder))
+                {
+                    try
+                    {
+                        if (Path.GetExtension(filePath) == ".lphp")
+                        {
+                            using (var md5 = MD5.Create())
+                            {
+                                using (var stream = File.OpenRead(filePath))
+                                {
+                                    byte[] md5Bytes = md5.ComputeHash(stream);
+
+                                    string md5Hash = Encoding.UTF8.GetString(md5Bytes, 0, md5Bytes.Length);
+                                    if (!lphpFiles.ContainsKey(md5Hash))
+                                    {
+                                        foreach (var item in lphpFiles.Where(kvp => kvp.Value == null).ToList())
+                                            lphpFiles.Remove(item.Key);
+
+                                        foreach (KeyValuePair<string, string> entry in lphpFiles.ToArray())
+                                            if (entry.Value == filePath) lphpFiles[entry.Key] = null;
+
+                                        lphpFiles.Add(md5Hash, filePath);
+
+                                        Console.WriteLine($"Change detected in {filePath}!");
+                                        // Run LPHP-Converter
+                                        // -----
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch { }
+                }
+                Thread.Sleep(100);
+            }
         }
     }
 }
