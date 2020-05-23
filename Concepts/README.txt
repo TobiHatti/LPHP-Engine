e2f9b1716a8f10bfb77f5219b8f2cbd65fe43b55
TobiHatti <tobias.hattinger@gmx.at>
2020-05-23 18:26:57 +0200

    Initial commit
    Added concepts

---
+++ Concepts/README.txt
@@ -0,0 +1,6 @@
+These files showcase the goal of LPHP:
+
+LPHP-Files get converted into PHP-Files after beeing processed.
+The Layout-Structure is similar to the one used with ASP.NET MVC - Razor
+
+Compilation occurs upon saving the lphp-file.
---
+++ Concepts/sample.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<title>Start-Page</title>
+	<title>Start-Page</title> 	
+</head>
+<body>
+	<nav>
+		<!-- Sample-File inserted here -->
+	</nav>
+	<main>
+		<h1> This is a Sample Page</h1>
+
+
+		<h1>Welcome to Start-Page </h1> 
+
+
+		<h1>Welcome to Start-Page </h1> 
+
+		<?php $author = "Endev"; ?>
+
+		<p>
+		This is a sample text from <?= $author ?> 
+		</p>
+	</main>
+	<footer>
+
+		<span>Contact: XXXXXXXXXXX</span>
+
+	</footer>
+</body>
+</html>
